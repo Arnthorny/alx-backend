ebe520337a7383d640634085f8318257c2fb5c2e
T <chidubem.anthony26@gmail.com>
2024-08-16 10:16:10 +0100

    Delete 0x03-queuing_system_in_js directory

--- 0x03-queuing_system_in_js/0-redis_client.js
+++
@@ -1,4 +0,0 @@
-import redis from 'redis';
-
-const redisCli = redis.createClient().on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
-redisCli.on('ready', () => console.log('Redis client connected to the server'));
