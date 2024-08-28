319c5679c0460093319d7bbea56de1054aae2b47
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-28 10:45:13 -0700

    chore: All tasks excluding tests, for 0x03-queuing system in js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,18 @@
+import redis from 'redis';
+
+const client = redis.createClient().on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+  // redis.print
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => console.log(res));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+displaySchoolValue('Hol');
